d99d540fea64a651e2b1c055617c0d4d604b56d1
renamcomn <acidmud@gmail.com>
2024-06-29 20:53:11 -0300

    style from Listing Component

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -3,6 +3,7 @@ import { View, Text, FlatList, ListRenderItem, StyleSheet, Image } from 'react-n
 import { defaultStyles } from '@/constants/Styles';
 import { Link } from 'expo-router';
 import { TouchableOpacity } from '@gorhom/bottom-sheet';
+import { Ionicons } from '@expo/vector-icons';
 interface Props {
     listings: any[];
     category: string;
@@ -13,7 +14,6 @@ const Listings = ({ listings: items, category } : Props) => {
   const listRef = useRef<FlatList>(null);
 
   useEffect(() => {
-    console.log('RELOAD LISTING: ', items.length)
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
@@ -26,6 +26,21 @@ const Listings = ({ listings: items, category } : Props) => {
       <TouchableOpacity>
         <View style={styles.listing}>
           <Image source={{ uri: item.medium_url}} style={styles.image} />
+          <TouchableOpacity style={{ position: 'absolute', right: 30, top: 30 }}>
+            <Ionicons name='heart-outline' size={24} color={'#000'} />
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 16, fontFamily: 'mon-sb' }}>{item.name}</Text>
+            <View style={{ flexDirection: 'row', gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: 'mon-sb' }}>{item.review_scores_rating / 20}</Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: 'mon' }}>{item.room_type}</Text>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+            <Text style={{ fontFamily: 'mon' }}>night</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </Link>
