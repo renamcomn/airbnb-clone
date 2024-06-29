5b85dd033c7d71028cc36c6c0ed68d8f2c0b69db
renamcomn <acidmud@gmail.com>
2024-06-29 17:20:03 -0300

    created login page and implements OAuth

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,118 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, TouchableOpacity, StyleSheet, TextInput, Text } from 'react-native'
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useRouter } from 'expo-router';
+
+enum Strategy {
+  Google = 'oauth_google',
+  Apple = 'oauth_apple',
+  Linkedin = 'oauth_linkedin'
+}
 
 const Page = () => {
+  useWarmUpBrowser();
+  const router = useRouter();
+
+  const { startOAuthFlow: appleAuth } = useOAuth({strategy: 'oauth_apple'});
+  const { startOAuthFlow: googleAuth } = useOAuth({strategy: 'oauth_google'});
+  const { startOAuthFlow: linkedinAuth } = useOAuth({strategy: 'oauth_linkedin'});
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Google]: googleAuth,
+      [Strategy.Linkedin]: linkedinAuth
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+      console.log(createdSessionId)
+
+      if(createdSessionId) {
+        setActive!( { session: createdSessionId })
+        router.back();
+      }
+    } catch (err) {
+      console.error('OAuth error: ', err)
+    }
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput 
+        autoCapitalize='none' 
+        placeholder='E-mail' 
+        style={[defaultStyles.inputField, { marginBottom: 30 }]} 
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View style={{ flex: 1, borderBottomColor: '#000', borderBottomWidth: StyleSheet.hairlineWidth}} />
+        <Text style={styles.separator}>or</Text>
+        <View style={{ flex: 1, borderBottomColor: '#000', borderBottomWidth: StyleSheet.hairlineWidth}} />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name='call-outline' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+          <Ionicons name='logo-apple' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Google)}>
+          <Ionicons name='logo-google' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Linkedin)}>
+          <Ionicons name='logo-linkedin' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Linkedin</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+
+  separatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+});
 
 export default Page
